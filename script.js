b5286d51fdcfd97dfd4b7ee0052dbfedd278427a
Kavyansh-Bagdi <kavyanshbagdi224@gmail.com>
2024-06-12 17:22:04 +0530

    tech stack added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let flag = 0; //light theme
 function changetheme() {
     if (flag == 0) {
         document.getElementById('theme').innerHTML = "Light Theme";
-        document.documentElement.style.setProperty('--textcolor', 'white');
+        document.documentElement.style.setProperty('--textcolor', 'rgba(255,255,255,0.8');
         document.documentElement.style.setProperty('--bgcolor', 'hsl(0, 0%, 9%)');
         document.documentElement.style.setProperty('--ulcolor', 'hsl(0, 0%, 50%)');
         document.documentElement.style.setProperty('--bordercolor', 'rgba(255,255,255,0.5)');
